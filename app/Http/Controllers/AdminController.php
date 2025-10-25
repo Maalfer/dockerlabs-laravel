d58de2297bd48b172b141b5f2fd5485041ac67ce
maalfer <marioalvarezfdz@gmail.com>
2025-10-25 10:33:46 +0200

    bunkerlabs

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Maquina;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('admin'); // o la vista correspondiente
+        $maquinas = Maquina::latest()->get();
+        return view('admin', compact('maquinas'));
     }
 }
